9be246888644e1ba291c8b006641c1843aaf6d85
FERBIN12 <fjferbin@gmail.com>
2025-05-25 21:23:57 +0530

    firmware

--- bumperbot_firmware/include/bumperbot_firmware/bumperbot_interface.hpp
+++ bumperbot_firmware/include/bumperbot_firmware/bumperbot_interface.hpp
@@ -1,43 +1,47 @@
-#pragma once
+#ifndef BUMPERBOT_INTERFACE_HPP
+#define BUMPERBOT_INTERFACE_HPP
 
 #include <rclcpp/rclcpp.hpp>
 #include <hardware_interface/system_interface.hpp>
+#include <libserial/SerialPort.h>
+#include <rclcpp_lifecycle/state.hpp>
 #include <rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp>
 
-#include <string>
 #include <vector>
+#include <string>
 
-#include <libserial/SerialPort.h>
 
+namespace bumperbot_firmware
+{
 
-namespace bumperbot_interface{
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-using callback_return = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
-class BumperbotInterface: public hardware_interface::SystemInterface
-{   
+class BumperbotInterface : public hardware_interface::SystemInterface
+{
 public:
-    BumperbotInterface();
-    virtual ~BumperbotInterface();
+  BumperbotInterface();
+  virtual ~BumperbotInterface();
 
-    virtual callback_return on_activate(const rclcpp_lifecycle::State & prev_state) override;
-    virtual callback_return on_deactivate(const rclcpp_lifecycle::State & prev_state) override;
+  // Implementing rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface
+  CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
 
-    virtual callback_return on_init(const hardware_interface::HardwareInfo & hardware_info) override;
-    virtual std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
-    virtual std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+  // Implementing hardware_interface::SystemInterface
+  CallbackReturn on_init(const hardware_interface::HardwareInfo &hardware_info) override;
+  std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
+  std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
+  hardware_interface::return_type read(const rclcpp::Time &, const rclcpp::Duration &) override;
+  hardware_interface::return_type write(const rclcpp::Time &, const rclcpp::Duration &) override;
 
-    virtual hardware_interface::return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
-    virtual hardware_interface::return_type write(const rclcpp::Time & time, const rclcpp::Duration & period) override;
 private:
-    LibSerial::SerialPort arduino_;
-
-    std::string port_;
-    std::vector<double> velocity_commands_;
-    std::vector<double> position_states_;
-    std::vector<double> velocity_states_;
-
-    rclcpp::Time last_run_ ;
-
+  LibSerial::SerialPort arduino_;
+  std::string port_;
+  std::vector<double> velocity_commands_;
+  std::vector<double> position_states_;
+  std::vector<double> velocity_states_;
+  rclcpp::Time last_run_;
 };
+}  // namespace bumperbot_firmware
 
-}   
+
+#endif  // BUMPERBOT_INTERFACE_HPP
