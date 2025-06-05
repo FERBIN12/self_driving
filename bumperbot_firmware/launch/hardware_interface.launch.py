c05ef93b349c2ba1df77e0bb43d5dfeff05efb37
FERBIN12 <fjferbin@gmail.com>
2025-06-05 16:21:06 +0530

    updated_hardware

--- bumperbot_firmware/launch/hardware_interface.launch.py
+++ bumperbot_firmware/launch/hardware_interface.launch.py
@@ -5,7 +5,6 @@ from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
 
     robot_description = ParameterValue(
